ac634032e7d887ac7efda1309fe3595a4deb786c
Hyun Seok Park <luckseok1@gmail.com>
2025-05-07 14:30:20 +0900

    fix: 네이버 클라우드 스토리지 image 세팅 #3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['kr.object.ncloudstorage.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'kr.object.ncloudstorage.com',
+        port: '',
+        pathname: '/hobbi-img/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
