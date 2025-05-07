b992d3e9ce4d6f0a3c3e2bf2a622f93fcb5269b0
Hyun Seok Park <luckseok1@gmail.com>
2025-05-07 16:19:05 +0900

    design: tab bar height 설정 #19

--- src/components/common/tab_bar.tsx
+++ src/components/common/tab_bar.tsx
@@ -18,56 +18,57 @@ export default function TabBar() {
       : 'text-[var(--grayscale-40)]';
 
   return (
-    <div className="w-[198px] h-[1024px] bg-white sticky top-0">
+    <div className="w-[198px] h-screen bg-white sticky top-0 flex flex-col">
       <div className="w-[198px] h-[112px] pt-[12px] pb-[5px] flex justify-center items-center">
         <SvgIcon name="logo" width={150} height={44} />
       </div>
+      <div className="flex-1 flex flex-col justify-between">
+        <div className="w-[198px] h-[412px] flex flex-col justify-between px-6 mt-[24px]">
+          <div className="w-[150px] flex items-center h-[52px] pt-[20px]">
+            <SvgIcon name="home" size={36} color={iconColor(isHome)} />
+            <span className={`ml-[24px] text-[16px] ${textColor(isHome)}`}>
+              홈
+            </span>
+          </div>
 
-      <div className="w-[198px] h-[412px] flex flex-col justify-between px-6 mt-[24px] mb-[411px]">
-        <div className="w-[150px] flex items-center h-[52px] pt-[20px]">
-          <SvgIcon name="home" size={36} color={iconColor(isHome)} />
-          <span className={`ml-[24px] text-[16px] ${textColor(isHome)}`}>
-            홈
-          </span>
+          <div className="w-[150px] flex items-center h-[52px]">
+            <SvgIcon name="search" size={36} color="#999999" />
+            <span className="ml-[24px] text-[16px] text-[var(--grayscale-40)]">
+              검색
+            </span>
+          </div>
+
+          <div className="w-[150px] flex items-center h-[52px]">
+            <SvgIcon name="alarm" size={36} color="#999999" />
+            <span className="ml-[24px] text-[16px] text-[var(--grayscale-40)]">
+              알림
+            </span>
+          </div>
+
+          <div
+            className="w-[150px] flex items-center h-[52px] cursor-pointer"
+            onClick={() => router.push('/posts/write')}
+          >
+            <SvgIcon name="write" size={36} color={iconColor(isWrite)} />
+            <span className={`ml-[24px] text-[16px] ${textColor(isWrite)}`}>
+              게시글 작성
+            </span>
+          </div>
+
+          <div
+            className="w-[150px] flex items-center h-[52px] cursor-pointer pb-[20px]"
+            onClick={() => router.push('/my_page')}
+          >
+            <SvgIcon name="my_page" size={36} color={iconColor(isMyPage)} />
+            <span className={`ml-[24px] text-[16px] ${textColor(isMyPage)}`}>
+              마이페이지
+            </span>
+          </div>
         </div>
 
-        <div className="w-[150px] flex items-center h-[52px]">
-          <SvgIcon name="search" size={36} color="#999999" />
-          <span className="ml-[24px] text-[16px] text-[var(--grayscale-40)]">
-            검색
-          </span>
+        <div className="w-[198px] h-[64px] flex items-center justify-end text-[16px] text-right pr-6">
+          <span className="text-[var(--grayscale-60)]">로그인</span>
         </div>
-
-        <div className="w-[150px] flex items-center h-[52px]">
-          <SvgIcon name="alarm" size={36} color="#999999" />
-          <span className="ml-[24px] text-[16px] text-[var(--grayscale-40)]">
-            알림
-          </span>
-        </div>
-
-        <div
-          className="w-[150px] flex items-center h-[52px] cursor-pointer"
-          onClick={() => router.push('/posts/write')}
-        >
-          <SvgIcon name="write" size={36} color={iconColor(isWrite)} />
-          <span className={`ml-[24px] text-[16px] ${textColor(isWrite)}`}>
-            게시글 작성
-          </span>
-        </div>
-
-        <div
-          className="w-[150px] flex items-center h-[52px] cursor-pointer pb-[20px]"
-          onClick={() => router.push('/my_page')}
-        >
-          <SvgIcon name="my_page" size={36} color={iconColor(isMyPage)} />
-          <span className={`ml-[24px] text-[16px] ${textColor(isMyPage)}`}>
-            마이페이지
-          </span>
-        </div>
-      </div>
-
-      <div className="w-[198px] h-[64px] flex items-center justify-end text-[16px] text-right pr-[24px]">
-        <span className="text-[var(--grayscale-60)]">로그인</span>
       </div>
     </div>
   );
