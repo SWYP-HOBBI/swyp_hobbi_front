3c7059bcfe5997f33dd7af57cdb30323180148b9
SoMin-Yoo <somin3994@gmail.com>
2025-05-16 01:53:50 +0900

    feat: 마이페이지 layout에 전역 코드 추가

--- src/app/my_page/layout.tsx
+++ src/app/my_page/layout.tsx
@@ -1,16 +1,32 @@
+'use client';
+
+import Modal from '@/components/common/modal';
 import TabBar from '@/components/common/tab_bar';
+import NotificationPage from '@/components/notification/notification_page';
+import Search from '@/components/search/search';
 import Providers from '@/services/providers';
+import { useNotificationStore } from '@/store/notification';
 
 export default function MypageLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { isNotificationOpen } = useNotificationStore();
   return (
     <Providers>
-      <div className="flex">
+      <div className="flex relative">
         <TabBar />
-        {children}
+        <div className="flex-1 bg-grayscale-1">
+          {children}
+          <Search />
+          {isNotificationOpen && (
+            <div className="fixed left-[200px] z-[9999]">
+              <NotificationPage />
+            </div>
+          )}
+        </div>
+        <Modal />
       </div>
     </Providers>
   );
