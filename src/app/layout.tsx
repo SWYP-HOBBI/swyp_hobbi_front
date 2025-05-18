a8b844910e4c4de2541fb8d4092fda1f7b5fd54b
Hyun Seok Park <luckseok1@gmail.com>
2025-05-19 02:54:04 +0900

    desgin: font 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from 'next';
-// import localFont from 'next/font/local';
+import localFont from 'next/font/local';
 import Script from 'next/script';
 import './globals.css';
 import SSEHandler from '@/services/sse_handler';
 import Providers from '@/services/providers';
 
-// const pretendard = localFont({
-//   src: '../assets/fonts/PretendardVariable.woff2',
-//   display: 'swap',
-//   weight: '100 900',
-//   variable: '--font-pretendard',
-// });
+const pretendard = localFont({
+  src: '../assets/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '100 900',
+  variable: '--font-pretendard',
+});
 
 export const metadata: Metadata = {
   title: 'HOBBi',
