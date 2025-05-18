c366d7a7c25ef0598ae3c06d7aa105b25097949b
SoMin-Yoo <somin3994@gmail.com>
2025-05-18 23:11:32 +0900

    design: 추가 기능 준비중 #4

--- src/components/my_page/user_rank.tsx
+++ src/components/my_page/user_rank.tsx
@@ -1,12 +1,15 @@
 export default function UserRank() {
   return (
-    <div className="text-[20px] font-semibold leading-[100%] mb-[23px]">
-      나의랭크
-      <div className="flex justify-center items-center mt-4">
+    <>
+      <div className="text-[20px] font-semibold leading-[100%] mb-[23px]">
+        나의 랭크
+        {/* <div className="flex justify-center items-center mt-4">
         <div className="w-[100px] h-[100px] bg-[#D9D9D9] rounded-full mr-[12px]"></div>
         <div className="text-[18px] leading-[100%] mr-[24px]">Lv. 고인물</div>
         <div className="w-[445px] h-[130px] bg-[#D9D9D9] rounded-[24px]"></div>
+      </div> */}
       </div>
-    </div>
+      <span>기능 준비 중..</span>
+    </>
   );
 }
