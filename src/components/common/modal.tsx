3d379e09f6ce540731a880480d3e075fba0efe26
Hyun Seok Park <luckseok1@gmail.com>
2025-05-20 20:00:38 +0900

    fix: modal tab_bar 여부에 따라 스타일 조정

--- src/components/common/modal.tsx
+++ src/components/common/modal.tsx
@@ -3,6 +3,7 @@
 import { useModalStore } from '@/store/modal';
 import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
+import { usePathname } from 'next/navigation';
 import Button from './button';
 
 export default function Modal() {
@@ -19,6 +20,11 @@ export default function Modal() {
     hideButtons,
     closeModal,
   } = useModalStore();
+
+  const pathname = usePathname();
+  const shouldAddPadding =
+    pathname === '/posts' || // 메인 피드
+    pathname === '/my-page'; // 마이페이지
 
   useEffect(() => {
     if (isOpen) {
@@ -51,14 +57,18 @@ export default function Modal() {
   };
 
   return createPortal(
-    <div className="fixed inset-0 z-50 flex items-center justify-center">
+    <div
+      className={`fixed inset-0 z-50 flex items-center justify-center ${shouldAddPadding ? 'md:pl-[280px]' : ''}`}
+    >
       <div className="absolute inset-0 bg-grayscale-100/50" />
-      <div className="space-y-6 p-11 relative bg-grayscale-0 rounded-xl w-full max-w-[360px] flex flex-col items-center text-center shadow-md">
+      <div className="space-y-6 p-11 relative bg-grayscale-0 rounded-xl w-full max-w-[360px] flex flex-col items-center text-center shadow-md mx-4 md:mx-0">
         <div
           className={`font-semibold text-xl ${type === 'error' ? 'text-like' : 'text-grayscale-100'}`}
         >
           {title && <h2>{title}</h2>}
-          {message && <p className="whitespace-pre-line">{message}</p>}
+          {message && (
+            <p className="whitespace-pre-line text-base mt-2">{message}</p>
+          )}
         </div>
         {!hideButtons && (
           <div className={`w-full ${showCancelButton ? 'flex gap-3' : ''}`}>
