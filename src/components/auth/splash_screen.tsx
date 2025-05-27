bdd5d017b562517189000334cb2d92082cf45d6f
Hyun Seok Park <luckseok1@gmail.com>
2025-05-27 21:36:58 +0900

    desgin: splash 화면 애니메이션

--- src/components/auth/splash_screen.tsx
+++ src/components/auth/splash_screen.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 import SvgIcon from '../common/svg_icon';
+import { motion, AnimatePresence } from 'framer-motion';
 
 /**
  * 스플래시 화면 컴포넌트 Props 인터페이스
@@ -14,9 +15,9 @@ interface SplashScreenProps {
  * 스플래시 화면 컴포넌트
  *
  * 주요 기능
- * 1. 2초 후에 애니메이션 시작
- * 2. 3초 후에 스플래시 스크린을 닫기
- * 3. 애니메이션 종료 후 스플래시 화면 종료
+ * 1. 로고가 페이드인되며 등장
+ * 2. 2초 후에 위로 이동하는 애니메이션 시작
+ * 3. 3초 후에 스플래시 스크린을 닫기
  */
 export default function SplashScreen({ onFinish }: SplashScreenProps) {
   const [mounted, setMounted] = useState<boolean>(false); // 컴파운드 컴포넌트 마운트 여부
@@ -61,16 +62,24 @@ export default function SplashScreen({ onFinish }: SplashScreenProps) {
   if (!portalElement) return null;
 
   return createPortal(
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-grayscale-0">
-      <div
-        className={`flex flex-col items-center transition-all duration-1000 ease-in-out
-          ${isAnimating ? 'transform -translate-y-[38vh]' : ''}`}
-      >
-        <SvgIcon
-          name="logo"
-          className="w-[240px] h-[70px] max-md:w-[150px] max-md:h-[44px]"
-        />
-      </div>
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-grayscale-100">
+      <AnimatePresence>
+        <motion.div
+          className={`flex flex-col items-center transition-all duration-1000 ease-in-out
+            ${isAnimating ? 'transform -translate-y-[38vh]' : ''}`}
+          initial={{ opacity: 0, scale: 0.3 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            ease: [0.43, 0.13, 0.23, 0.96],
+          }}
+        >
+          <SvgIcon
+            name="logo"
+            className="w-[240px] h-[70px] max-md:w-[150px] max-md:h-[44px]"
+          />
+        </motion.div>
+      </AnimatePresence>
     </div>,
     portalElement,
   );
