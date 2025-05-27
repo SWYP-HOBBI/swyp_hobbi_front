85a67a2d0e366607d7d5896aa0d52c7d9f7a0c51
Hyun Seok Park <luckseok1@gmail.com>
2025-05-27 18:27:53 +0900

    test: 소셜 로그인 구현 테스트 진행중 #148

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,14 @@
+import Modal from '@/components/common/modal';
+
+export default function LoginLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="max-w-3xl mx-auto flex flex-col items-center justify-center min-h-screen p-4 bg-grayscale-0">
+      {children}
+      <Modal />
+    </div>
+  );
+}
