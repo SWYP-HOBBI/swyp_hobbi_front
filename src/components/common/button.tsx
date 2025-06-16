f179826244f226971cda652108409babe90a3df7
Hyun Seok Park <luckseok1@gmail.com>
2025-06-16 21:59:18 +0900

    feat: button storybook #183

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -1,7 +1,22 @@
 import React, { ButtonHTMLAttributes } from 'react';
+import clsx from 'clsx';
 
 type ButtonVariant = 'primary' | 'outline' | 'secondary';
 type ButtonSize = 'sm' | 'md' | 'lg' | 'xl';
+
+const sizeStyles = {
+  sm: 'text-sm',
+  md: 'text-base',
+  lg: 'text-lg',
+  xl: 'text-xl',
+};
+
+const variantStyles = {
+  primary: 'bg-primary text-primary-b80 hover:bg-primary/80',
+  outline:
+    'border border-primary-b60 bg-grayscale-0 text-primary-b60 hover:bg-primary-b60 hover:text-grayscale-0',
+  secondary: 'bg-grayscale-10 text-grayscale-50',
+};
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: ButtonVariant;
@@ -21,31 +36,17 @@ export default function Button({
   disabled = false,
   ...props
 }: ButtonProps) {
-  const baseStyle =
-    'py-3 px-4 rounded-lg button_transition font-semibold text-center cursor-pointer disabled:cursor-not-allowed h-[60px] max-md:h-[48px] whitespace-nowrap';
-
-  const sizeStyles = {
-    sm: 'text-sm',
-    md: 'text-base',
-    lg: 'text-lg',
-    xl: 'text-xl',
-  };
-
-  // disabled일 때는 secondary 스타일 적용
   const currentVariant = disabled ? 'secondary' : variant;
-
-  const variantStyles = {
-    primary: 'bg-primary text-primary-b80 hover:bg-primary/80',
-    outline:
-      'border border-primary-b60 bg-grayscale-0 text-primary-b60 hover:bg-primary-b60 hover:text-grayscale-0',
-    secondary: 'bg-grayscale-10 text-grayscale-50',
-  };
-
-  const widthStyle = fullWidth ? 'w-full' : '';
 
   return (
     <button
-      className={`${baseStyle} ${sizeStyles[size]} ${variantStyles[currentVariant]} ${widthStyle} ${className}`}
+      className={clsx(
+        'py-3 px-4 rounded-lg button_transition font-semibold text-center cursor-pointer disabled:cursor-not-allowed h-[60px] max-md:h-[48px] whitespace-nowrap',
+        sizeStyles[size],
+        variantStyles[currentVariant],
+        { 'w-full': fullWidth },
+        className,
+      )}
       disabled={disabled}
       {...props}
     >
